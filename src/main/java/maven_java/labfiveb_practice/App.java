dd9d5fcd5d8bebf91f2f44f0fb7f57c026b07f05
VarunReddyT <tvarun2014@gmail.com>
2024-12-29 17:38:13 +0530

    First

---
+++ src/main/java/maven_java/labfiveb_practice/App.java
@@ -0,0 +1,10 @@
+package maven_java.labfiveb_practice;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
